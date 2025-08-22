02dd101eb481f1c94da6efc88232cc8c1fad32d8
Luis Rodriguez <cuentadeluis27@gmail.com>
2025-08-22 12:22:14 -0600

    feat: toast notifications

--- renderer/src/routes/__root.tsx
+++ renderer/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Sidebar from '@/components/layout/Sidebar'
+import { Toaster } from 'sonner'
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,6 +9,12 @@ export const Route = createRootRoute({
       <Sidebar />
       <main className="flex-1 overflow-auto">
         <Outlet />
+        <Toaster 
+          position="top-right"
+          richColors
+          expand={true}
+          duration={4000}
+        />
       </main>
       <TanStackRouterDevtools />
     </div>
