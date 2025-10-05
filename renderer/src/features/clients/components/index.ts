9699a727ce72c47fdd3e1f3df564f7f258c7cbe3
Luis Rodriguez <cuentadeluis27@gmail.com>
2025-10-04 23:15:59 -0600

    feat: Export ClientBudgetModal in index.ts for component accessibility

--- renderer/src/features/clients/components/index.ts
+++ renderer/src/features/clients/components/index.ts
@@ -5,3 +5,4 @@ export { default as ColorSelector } from './ColorSelector';
 export { default as ClientColorIndicator } from './ClientColorIndicator';
 export { default as ClientOrdersModal } from './ClientOrdersModal';
 export { default as ClientPaymentsModal } from './ClientPaymentsModal';
+export { default as ClientBudgetModal } from './ClientBudgetModal';
