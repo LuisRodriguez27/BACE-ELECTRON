718604f369c4b26d36e5647eac32b1465d3c8cb3
Luis Rodriguez <cuentadeluis27@gmail.com>
2025-12-27 12:55:21 -0600

    feat: adjust dependencies for Windows 7 support (react)

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import path from 'path'
 
@@ -8,7 +7,6 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     TanStackRouterVite(),
   ],
   resolve: {
