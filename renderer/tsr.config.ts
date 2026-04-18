58115fc58d5e73fbd6b08d92aba87245fa183d1f
Luis Rodriguez <cuentadeluis27@gmail.com>
2026-04-18 15:23:55 -0600

    chore: bump version to 5.1.4 and update tsconfig and tsr.config

--- renderer/tsr.config.ts
+++ renderer/tsr.config.ts
@@ -1,3 +1,3 @@
-import { defineConfig } from '@tanstack/router-vite-plugin'
+import { type Config } from '@tanstack/router-vite-plugin'
 
-export default defineConfig({})
+export default {} satisfies Partial<Config>
