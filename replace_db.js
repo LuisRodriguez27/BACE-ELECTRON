927f0779d223b07890ef07cea4b6ccfb4c1e517d
Luis Rodriguez <cuentadeluis27@gmail.com>
2026-04-21 23:06:50 -0600

    chore: remove temporary migration and refactoring scripts

--- replace_db.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('/home/luis/Projects/BACE-ELECTRON/electron/db.js', 'utf8');
-content = content.replace(/active INTEGER NOT NULL DEFAULT 1/g, 'active BOOLEAN NOT NULL DEFAULT TRUE');
-fs.writeFileSync('/home/luis/Projects/BACE-ELECTRON/electron/db.js', content);
